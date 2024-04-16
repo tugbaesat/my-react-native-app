72a7ae6305bad1e04ac71ad1685df92b17d0905c
Tugba Esat <tugbaesat007@gmail.com>
2024-04-16 12:14:43 -0700

    components added

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
+
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>StyleSheet</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightblue",
+    padding: 60,
+  },
+  title: {
+    color: "white",
   },
 });
