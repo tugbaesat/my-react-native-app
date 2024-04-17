13f9fdab16150f65192fe34d77af9621ce0f3e61
Tugba Esat <tugbaesat007@gmail.com>
2024-04-17 12:37:52 -0700

    add screens

--- App.js
+++ App.js
@@ -1,39 +1,22 @@
-import { View, Text, StyleSheet, Dimensions } from "react-native";
-import DimensionsBox from "./components/DimensionsBox";
-import CustomButton from "./components/CustomButton/CustomButton";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import AboutScreen from "./screens/AboutScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome!</Text>
-        <CustomButton title="Press Me" onPress={() => alert("Pressed!")} />
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome Home" }}
+        />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 64,
-    borderWidth: 6,
-
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  box: {
-    backgroundColor: "lightblue",
-    padding: 20,
-    width: windowWidth > 500 ? "70%" : "90%",
-    height: windowHeight > 500 ? "60%" : "90%",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: windowWidth > 500 ? 24 : 50,
-  },
-});
